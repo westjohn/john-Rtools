11fad7f5961b422718b4856a677d92942490d559
hpcjohn <john.e.west@gmail.com>
2015-10-19 21:52:47 -0400

    Add code to allow user to specify population breaks for plotting

--- R/quant_plot.R
+++ R/quant_plot.R
@@ -4,64 +4,63 @@
 #' creates a quantile plot of the data. This function requires
 #' GGally, ggplot2, reshape2 (probably)
 #'
-#' @param data An atomic vector of numbers
+#' @param d An atomic vector of numbers to examine using the quantile plot
 #' @param ylabel Label for the quantity being plotted; defaults to "Quantity"
-#' @param quantile Vector of numeric vector of probabilities with values in [0,1]
+#' @param quantiles Vector of numeric vector of probabilities with values in [0,1]
 #' representing the quantile boundaries. This defaults to quartiles, e.g.: seq(0,1,by=.25).
+#' @param color R color name (e.g., "blue") in which to render the colors indicating the quantiles; defaults to "red"
 #' @keywords quantile plot, quant_plot
 #' @export
 #' @examples
-#' quant_plot(data=df$height,ylabel="Height")
+#' quant_plot(d=df$height,ylabel="Height")
+#' quant_plot(d=rnorm(1000),ylabel="Normal Distribution",quantiles=c(0,.33,.667,1),color="blue")
 
 
-#library(ggplot2)
-#library(ggthemes)
-#library(plyr)
-#library(dplyr)
-#library(xlsx)
-#library(reshape2)
-#library(GGally)
-
-#This creates a quartile plot of the data with lines at 25%, 50%, and 75%
-quant_plot <- function(d,ylabel="Quantity",quantile=seq(0,1,by=0.25)) {
+quant_plot <- function(d,ylabel="Quantity",quantiles=seq(0,1,by=0.25),color = "red") {
     d <- sort(d)
-    q <- data.frame(quantile(d))
-    names(q)<-"breaks"
-    X<- seq(from=1,to=length(d))
+    quantile_data <- data.frame(breaks=quantile(d,probs = quantiles))
+    quantile_data$quantiles <- rownames(quantile_data)
+    rownames(quantile_data) <- NULL
+
+    X <- seq(from=1,to=length(d))
     d <- data.frame("data"=d,X)
-
-    q25 <- d[max(which(d$data<=q["25%","breaks"])),"data"]
-    q50 <- d[max(which(d$data<=q["50%","breaks"])),"data"]
-    q75 <- d[max(which(d$data<=q["75%","breaks"])),"data"]
-    q100 <- d[max(which(d$data<=q["100%","breaks"])),"data"]
-#     print(d[max(which(d$data>=.75)),"data"])
-#     print(nrow(d))
-
-    plt <- ggplot(data=d,aes(y=data,x=X)) +
-        geom_point(size=2,alpha=.2,shape=20,size=2) +
-        geom_hline(data=data.frame(),yintercept=q25,color="white") +
-        geom_hline(data=data.frame(),yintercept=q50,color="white") +
-        geom_hline(data=data.frame(),yintercept=q75,color="white") +
-        geom_hline(data=data.frame(),yintercept=q100,color="white") +
-#         geom_hline(data=data.frame(),yintercept=.75,color="white") +
-#         geom_text(data=data.frame(),aes(x=1,y=q25,label="25%"),size=3) +
-#         geom_text(data=data.frame(),x=1,y=q50,label="50%",size=3) +
-#         geom_text(data=data.frame(),x=1,y=q75,label="75%",size=3) +
-#         geom_text(data=data.frame(),x=1,y=q100,label="100%",size=3) +
-#         geom_text(data=data.frame(),aes(x=1,y=4.24/32,label="25%"),size=3) +
-#         geom_text(data=data.frame(),aes(x=1,y=5.72/32,label="50%"),size=3) +
-#         geom_text(data=data.frame(),aes(x=1,y=8.86/32,label="75%"),size=3) +
-#         geom_text(data=data.frame(),aes(x=1,y=31.32/32,label="100%"),size=3) +
-        ggtitle(paste("Quartile Plot for ",ylabel,collapse = "",sep = "")) +
-        scale_y_continuous(breaks=c(q25,q50,q75,q100),
-                           labels=c(sprintf("%.2f",q25*32.),
-                                    sprintf("%.2f",q50*32.),
-                                    sprintf("%.2f",q75*32.),
-#                                    sprintf("%.2f",.75*32.),
-                                    sprintf("%.2f",q100*32.))) +
+    
+    if(nrow(d) > 5000) {
+        PT_ALPHA <- .075
+    }else if(nrow(d) > 999) {
+        PT_ALPHA <- .1
+    }else if(nrow(d) >499) {
+        PT_ALPHA <- .5
+    }else if(nrow(d) >99) {
+        PT_ALPHA <- .75
+    }else if(nrow(d) >9) {
+        PT_ALPHA <- 1.
+    }
+    else {
+        PT_ALPHA <- 1.
+    }
+    
+    ALPHA_VAL <- .2
+    
+    plt <- ggplot(data=d,aes(y=data,x=X))
+    for (bk in 1:(nrow(quantile_data)-1)) {
+        plt <- plt + annotate("rect",xmin=min(which(d$data<=quantile_data[bk,"breaks"])),
+                        ymin=d[min(which(d$data<=quantile_data[bk,"breaks"])),"data"],
+                        xmax=max(which(d$data<=quantile_data[bk+1,"breaks"])),
+                        ymax=d[max(which(d$data<=quantile_data[bk+1,"breaks"])),"data"],fill=color,alpha=ALPHA_VAL)
+    }
+    plt <- plt + scale_y_continuous(breaks=quantile_data$breaks,
+                                    label=paste(" ",sprintf("%0.2f",quantile_data[,"breaks"]),
+                                                "::",quantile_data[,"quantiles"]))
+    plt <- plt + 
+        geom_point(size=2,alpha=PT_ALPHA,shape=20,size=2) +
+        ggtitle(paste("Quantile Plot for ",ylabel,collapse = "",sep = "")) +
         theme(panel.grid.major.y = element_blank(),panel.grid.major.x = element_blank()) +
         theme(panel.grid.minor.y = element_blank(),panel.grid.minor.x = element_blank()) +
+        theme(panel.background = element_blank()) +
+        theme(axis.ticks.y=element_blank()) +
+        theme(axis.text.y=element_text(hjust=1.75)) +
         ylab(ylabel) +
-        xlab("")
+        xlab("Index of Sorted Data Value")
     print(plt)
 }
